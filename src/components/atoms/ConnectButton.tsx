6ce6e947311617d003d3fab5de61d7e758b73505
Ab <abdila1099@gmail.com>
2025-12-02 17:53:54 +0100

    feat: enhance ConnectButton to display connection status message

--- src/components/atoms/ConnectButton.tsx
+++ src/components/atoms/ConnectButton.tsx
@@ -1,12 +1,15 @@
 import { deviceIp } from "@/types/adb";
-import { Button, ButtonProps, Tooltip } from "antd";
+import { Button, ButtonProps, message, Tooltip } from "antd";
 
 interface ConnectButtonProps extends ButtonProps {
     deviceIp: deviceIp;
 }
 export default function ConnectButton({ deviceIp, ...props }: ConnectButtonProps) {
     return <Tooltip title={`Connect to device at ${deviceIp}`}>
-        <Button  {...props}>
+        <Button  {...props} onClick={(e) => {
+            props?.onClick?.(e)
+            message.info(`Connecting to ${deviceIp}...`, 1.5)
+        }}>
             Connect
         </Button>
     </Tooltip>
