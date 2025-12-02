34559ff09e5c85c81a618eb42b75346520140253
Ab <abdila1099@gmail.com>
2025-12-02 17:25:26 +0100

    feat: add device fetching API and integrate axios in ConnectionManager

--- src/components/organisms/ConnectionManager.tsx
+++ src/components/organisms/ConnectionManager.tsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DevicesModal from "../atoms/DevicesModal";
 import ConnectionsButtons from "../molecules/ConnectionButtons";
 interface Props extends React.HTMLAttributes<HTMLDivElement> { }
 export default function ConnectionManager(props: Props) {
     const [open, setOpen] = useState(false);
+    const [devices, setDevices] = useState<string[]>([]);
+    const [loading, setLoading] = useState(false)
+    function fetchDevices() {
+        setLoading(true);
+        axios.get('/api/devices').then(res => {
+            setDevices(res.data.devices);
+        }).finally(() => {
+            setLoading(false);
+        })
+    }
+    useEffect(fetchDevices, [])
+    function closeModal() {
+        setOpen(false);
+    }
     return <div {...props}>
-        <ConnectionsButtons connected={false} connectButtonProps={{
-            onClick: () => {
-                setOpen(true)
-            }
-        }} />
-        <DevicesModal open={open} onCancel={() => {
-            setOpen(false)
-        }} onOk={() => {
-            setOpen(false)
-        }} devices={["test", "asd", "asd"]} />
+        <ConnectionsButtons connected={true}
+            connectButtonProps={{
+                loading: loading
+            }}
+            swithButtonProps={{
+                onClick: () => {
+                    setOpen(true)
+                },
+                loading: loading
+            }}
+        />
+        <DevicesModal loading={loading} open={open} onCancel={closeModal} onOk={closeModal} devices={devices} />
     </div>
 }
---
+++ src/pages/api/devices.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { execSync } from "child_process";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  devices: string[];
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  try {
+    // Fixed: Use double quotes for the outer bash command and single quotes for awk
+    const stdout = execSync(
+      `nmap -p 3000 --open 192.168.1.0/24 -oG - | grep "/open/" | awk '{print $2}'`,
+      { encoding: "utf-8" }
+    );
+
+    res.status(200).json({
+      devices: stdout.split("\n").filter(line => line.trim() !== "")
+    });
+  } catch (error: any) {
+    res.status(500).json({ devices: [] });
+    console.error("Error:", error.message);
+  }
+}
